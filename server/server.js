cbf1ea4be75e061da03f89e0a06b8e4bc228718e
Joe <josephsanglee@gmail.com>
2016-10-24 18:07:47 -0700

    Implement a basic Express server; Create User/Game models

--- server/server.js
+++ server/server.js
@@ -1,9 +1,26 @@
 var express = require('express');
+var Promise = require('bluebird');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var morgan = require('morgan');
 var methodOverride = require('method-override');
 var port = process.env.PORT || 8000;
 var app = express();
+
+/*************************DATABASE***************************/
+
+mongoose.connect('mongodb://localhost/baller');
+
+var User = require('./models/userModel.js');
+var findUser = Promise.promisify(User.findOne, User);
+var createUser = Promise.promisify(User.create, User);
+var findAllUsers = Promise.promisify(User.find, User);
+
+var Game = require('./models/gameModel.js');
+var findGame = Promise.promisify(Game.findOne, Game);
+var createGame = Promise.promisify(Game.create, Game);
+var findAllGames = Promise.promisify(Game.find, Game);
+
 
 /************************MIDDLEWARE**************************/
 
@@ -25,40 +42,68 @@ app.use(methodOverride('X-HTTP-Method-Override'));
 
 // handle user signin
 app.post('/api/signin', function(req, res) {
+  var user = req.body;
 
+  findUser({ email: user.email })
+  .then(function(currentUser) {
+    if (!currentUser) { return res.status(404).send('User does not exist.'); }
+
+    currentUser.comparePasswords(user.password)
+    .then(function(matched) {
+      if (matched) {
+        res.status(200).send('Signin successful!');
+      }
+    });
+  });
 });
 
 // handle user signup
 app.post('/api/signup', function(req, res) {
+  var user = req.body;
+  user.location = null;
 
-});
+  findUser({ email: user.email })
+  .then(function(currentUser) {
+    if (currentUser) { return res.status(404).send('User already exists'); }
+    createUser(user)
+    .then(function(user) {
+      if (!user) { return res.status(404).send('Invalid user creation'); } 
 
-// getting/posting users
-app.route('/api/users')
-  .get(function(req, res) {
-
-  })
-  .post(function(req, res) {
-
+      res.status(200).send('Successfully created!');
+    });
   });
-
-// getting/posting games
-app.route('/api/games')
-  .get(function(req, res) {
-
-  })
-  .post(function(req, res) {
-
-  });
-
-/*************************TEMPLATE ROUTES************************/
-app.get('/', function(req, res) {
-  //fill in with templates
-  //res.render ( STUFF );
 });
 
 
+// gets all users
+app.get('/api/users', function(req, res) {
+  findAllUsers({})
+  .then(function(users) {
+    res.status(200).send({users: users});
+  });
+});
 
+
+// gets all games
+app.get('/api/games', function(req, res) {
+  findAllGames({})
+  .then(function(games) {
+    res.status(200).send({games: games});
+  });
+});
+
+//post a game to the database
+app.post('/api/games', function(req, res) {
+  var game = req.body;
+  console.log(req.body);
+
+  createGame(game)
+    .then(function(game) {
+      if (!game) { return res.status(404).send('Invalid game creation'); } 
+
+      res.status(200).send('Game successfully created!');
+    });
+});
 
 /***********************SERVER START*************************/
 
