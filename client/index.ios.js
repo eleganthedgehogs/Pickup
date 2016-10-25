28362fa547ca1b9a9cbc05b12895df3d007f529d
Carlos Portillo <caportil@gmail.com>
2016-10-25 13:41:54 -0700

    Update client to correctly render base map via react-native-maps

--- client/index.ios.js
+++ client/index.ios.js
@@ -1,37 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React from 'react';
 import { AppRegistry } from 'react-native';
-import LogIn from './components/login/login';
-import Home from './components/home/home';
+import App from './App';
 
-export default class client extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {view: 'login'};
-	}
-
-  redirect(route) {
-  	console.log("setting state here");
-  	this.setState({view: route});
-  	console.log(this.state.view);
-  } 
-
+class AirMapsExplorer extends React.Component {
   render() {
-    if (this.state.view === 'login') {
-      return (
-        <LogIn redirect={this.redirect.bind(this)}/>
-      )      
-    } else if (this.state.view === 'home') {
-      return (
-        <Home redirect={this.redirect.bind(this)}/>
-      )
-    }
+    return <App />;
   }
 }
 
-AppRegistry.registerComponent('client', () => client);
+AppRegistry.registerComponent('AirMapsExplorer', () => AirMapsExplorer);
