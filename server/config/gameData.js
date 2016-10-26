4ea2d60563a3bc25b1564636705c7739b949de82
Joe <josephsanglee@gmail.com>
2016-10-26 16:06:58 -0700

    Grab court and and game data from server when main app page loads

---
+++ server/config/gameData.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    type: '3',
+    playerIds: ['112312', '123123123'],
+    time: 90,
+    court: 'Alamo Square'
+  },
+  {
+    type: '5',
+    playerIds: ['112123312', '123123123'],
+    time: 90,
+    court: 'Alice Marble'
+  },
+  {
+    type: 'open',
+    playerIds: ['112111123123312', '1223123123'],
+    time: 90,
+    court: 'Balboa Park'
+  }
+];
