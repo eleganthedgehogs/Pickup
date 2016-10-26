15bc00292524e30d6cbece793da67238c4d6e528
Vien Tang <vientang@gmail.com>
2016-10-26 12:48:49 -0700

    Refactor Map.js, Footer.js, Header.js, Login.js into separate components, ES5 => ES6, clean up unused import statements

--- client/components/maps/Map.js
+++ client/components/maps/Map.js
@@ -1,71 +1,52 @@
-import React from 'react';
-import {
-  StyleSheet,
-  View,
-  Dimensions,
-  SegmentedControlIOS
-} from 'react-native';
-
+import React, { Component } from 'react';
+import { View, Dimensions } from 'react-native';
 import MapView from'react-native-maps';
 import PriceMarker from './PriceMarker';
-import { Container, Header, Title, Text, Content, Footer, FooterTab, Button, Icon } from 'native-base';
-import FooterClass from '../footer/Footer';
+import { Container } from 'native-base';
+import Foot from '../footer/Footer';
+import styles from './styles';
+import Head from '../header/header';
 
 const { width, height } = Dimensions.get('window');
-
-const ASPECT_RATIO = width / height;
-const LATITUDE = 37.78825;
-const LONGITUDE = -122.4324;
-const LATITUDE_DELTA = 0.0922;
-const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const aspectRatio = width / height;
+const latitude = 37.78825;
+const longitude = -122.4324;
+const latitudeDelta = 0.0922;
+const longitudeDelta = latitudeDelta * aspectRatio;
 const SPACE = 0.01;
+const initialRegion = {latitude, longitude, latitudeDelta, longitudeDelta}
 
-function log(eventName, e) {
-  console.log(eventName, e.nativeEvent);
-}
-
-class HomeMap extends React.Component {
+class HomeMap extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      a: {
-        latitude: LATITUDE + SPACE,
-        longitude: LONGITUDE + SPACE,
-      },
-      b: {
-        latitude: LATITUDE - SPACE,
-        longitude: LONGITUDE - SPACE,
-      },
       games: [],
       selectedGame: false
     };
   }
 
   componentWillMount() {
+    // use AXIOS to grab data from backend
     this.state.games.push({
       players: [1,2,3,4,5],
       time: 90,
       court: 'Baller Court',
-      lat: LATITUDE + SPACE,
-      long: LONGITUDE - SPACE
+      lat: latitude + SPACE,
+      long: longitude - SPACE
     }, {
       players: [1,2,3,4,5,6,7,8],
       time: 30,
       court: 'Supa Court',
-      lat: LATITUDE - SPACE,
-      long: LONGITUDE + SPACE
+      lat: latitude - SPACE,
+      long: longitude + SPACE
     }, {
       players: [1,2],
       time: 120,
       court: 'Dupa Court',
-      lat: LATITUDE - SPACE - 0.01,
-      long: LONGITUDE + SPACE + 0.01
+      lat: latitude - SPACE - 0.01,
+      long: longitude + SPACE + 0.01
     })
-  }
-
-  chooseGame(game) {
-
   }
 
   render() {
@@ -76,63 +57,27 @@ class HomeMap extends React.Component {
           <MapView
             provider={this.props.provider}
             style={styles.map}
-            initialRegion={{
-              latitude: LATITUDE,
-              longitude: LONGITUDE,
-              latitudeDelta: LATITUDE_DELTA,
-              longitudeDelta: LONGITUDE_DELTA,
-            }}
-          >
-           <View style={styles.viewContainer}>
-            <Title style={styles.viewHeader}>Pickup</Title>
-            <View>
-              <SegmentedControlIOS style={styles.segmented} values={['Join', 'Create']} selectedIndex={0} />
-            </View>
-           </View>
+            initialRegion = {initialRegion}>
+           
+            <Head />
+
             {this.state.games.map((game, i) => (
-              <MapView.Marker onPress={(event) => {
-                  self.setState({selectedGame: game});
-              }} coordinate={{
-                latitude: game.lat,
-                longitude: game.long
-              }} key= {i}>
-                  <PriceMarker amount={game.players.length} countdown={game.time}/>
+              <MapView.Marker 
+                onPress={ event => self.setState({selectedGame: game}) }
+                coordinate={{latitude: game.lat, longitude: game.long}} 
+                key={i}>
+                  <PriceMarker 
+                    amount={game.players.length} 
+                    countdown={game.time}/>
               </MapView.Marker>
-          ))}
+            ))}
+
           </MapView>
-         <FooterClass game={self.state.selectedGame}/>
+         <Foot game={self.state.selectedGame}/>
         </View>
       </Container>
     );
   }
 }
-
-HomeMap.propTypes = {
-  provider: MapView.ProviderPropType,
-};
-
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-  viewContainer: {
-    backgroundColor: 'rgba(255,255,255,0.5)'
-  },
-  viewHeader: {
-    marginTop: 28
-  },
-  segmented: {
-    marginTop: 10,
-    marginBottom: 10,
-    backgroundColor: 'rgba(255,255,255,0.5)',
-    marginLeft: 5,
-    marginRight: 5
-  }
-});
 
 module.exports = HomeMap;
---
+++ client/components/maps/styles.js
@@ -0,0 +1,13 @@
+const React = require('react-native');
+const { StyleSheet } = React;
+
+module.exports = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  }
+});
