969c38d8311871af8e3f4b09781ba5afed6fe0b9
Carlos Portillo <caportil@gmail.com>
2016-10-27 17:42:26 -0700

    Update creategame functionality to pass up user selected picker data to parent component

--- client/components/TimePicker/TimePicker.js
+++ client/components/TimePicker/TimePicker.js
@@ -24,7 +24,7 @@ class TimePicker extends Component {
 	        date={this.state.date}
 	        mode="time"
 	        timeZoneOffsetInMinutes={this.state.timeZoneOffsetInHours * 60}
-	        onDateChange={date => this.setState({date: date}) }
+	        onDateChange={date => this.setState({date}, () => this.props.onTimeDataChange(this.state.date)) }
 	      />	
       </View>
 		)
@@ -38,4 +38,19 @@ export default TimePicker;
 To change the text color of DatePickerIOS, we need to refactor
 code in react-native node modules client. (10/26/2016)
 https://github.com/facebook/react-native/issues/4473
+
+1) Open Client/node_modules/react-native/React/Views/RCTDatePickerManager.m
+2) Replace lines 32-35 with the following:
+
+- (UIView *)view
+{
+ RCTDatePicker *datePicker = [RCTDatePicker new];
+ [datePicker setValue:[UIColor whiteColor] forKeyPath:@"textColor"];
+
+ return datePicker;
+}
+
+4) Save & rebuild simulator
+5) Profit
+
 **************************************************************/
