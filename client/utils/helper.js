a6e3e8662de32d546e5e289b2d326ebd5251969f
Carlos Portillo <caportil@gmail.com>
2016-10-28 16:12:10 -0700

    Add signup functionality writing to database

--- client/utils/helper.js
+++ client/utils/helper.js
@@ -9,6 +9,10 @@ let postLogin = function(user) {
   return axios.post('http://localhost:8000/api/login', user);
 };
 
+let postSignUp = function(user) {
+  return axios.post('http://localhost:8000/api/signup', user);
+};
+
 let postNewGame = function(game) {
 	console.log('need to post game here', game);
   return axios.post('http://localhost:8000/api/games', game);
@@ -19,6 +23,7 @@ let postNewGame = function(game) {
 var data = {
   getMainData: getMainData,
   postLogin: postLogin,
+  postSignUp:postSignUp,
   postNewGame: postNewGame
 };
 
