47d13d84a7be56afd04a680f00bb2912730ddec2
Joe <josephsanglee@gmail.com>
2016-10-28 18:47:28 -0700

    Start working on JoinGame functionality

--- client/components/JoinGame/JoinGame.js
+++ client/components/JoinGame/JoinGame.js
@@ -7,41 +7,41 @@ import Hr from 'react-native-hr';
 const joinGameBtnColor = '#20DA9B';
 
 class JoinGame extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {}
-	}
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-	render() {
-		return (
+  render() {
+    return (
 			<View style={styles.container}>
-			 <Button 
-			 	transparent
-			 	onPress={ () => this.props.exitJoinGame() } 
-			 	textStyle={styles.xtext} 
-			 	style={styles.x}>X</Button>
+        <Button 
+        transparent
+        onPress={ () => this.props.exitJoinGame() } 
+        textStyle={styles.xtext} 
+        style={styles.x}>X</Button>
 
-			 <View style={styles.innerContainer}>
-			  <Title style={styles.title}>Game Type</Title>
-			  <Hr lineColor='rgba(255, 255, 255, .5)'/>
-			  <Title style={styles.stats}>{this.props.game.type}</Title>
+        <View style={styles.innerContainer}>
+          <Title style={styles.title}>Game Type</Title>
+          <Hr lineColor='rgba(255, 255, 255, .5)'/>
+          <Title style={styles.stats}>{this.props.game.type}</Title>
 
-			  <Title style={styles.title}>Start Time</Title>
-			  <Hr lineColor='rgba(255, 255, 255, .5)'/>
-			  <Title style={styles.stats}>{this.props.game.time}</Title>
+          <Title style={styles.title}>Start Time</Title>
+          <Hr lineColor='rgba(255, 255, 255, .5)'/>
+          <Title style={styles.stats}>{this.props.game.time}</Title>
 
-			  <Title style={styles.title}>Number of Players</Title>
-			  <Hr lineColor='rgba(255, 255, 255, .5)'/>
-			  <Title style={styles.stats}>{this.props.game.playerIds.length}</Title>
+          <Title style={styles.title}>Number of Players</Title>
+          <Hr lineColor='rgba(255, 255, 255, .5)'/>
+          <Title style={styles.stats}>{this.props.game.playerIds.length}</Title>
 
-			  <Button 
-			  	block
-			  	onPress={ () => this.props.joinGame() }
-			  	style={styles.button}>Join Game!</Button>
-			 </View>
+          <Button 
+            block
+            onPress={ () => this.props.joinGame() }
+            style={styles.button}>Join Game!</Button>
+        </View>
 			</View>
-		)
-	}
+		);
+  }
 }
 
 export default JoinGame;
