3c181fc54b5bb93bedfbc59f0cfd4d93d092335b
Vien Tang <vientang@gmail.com>
2016-10-31 10:43:48 -0700

    Wallpaper for signup and login screen

--- client/components/signup/signup.js
+++ client/components/signup/signup.js
@@ -5,7 +5,7 @@ import styles from './styles';
 import helper from '../../utils/helper';
 
 var STORAGE_KEY = 'id_token';
-const background = require('../images/shadow.png');
+const background = require('../images/basketball_rim.jpg');
 
 class SignUp extends Component {
   constructor(props) {
